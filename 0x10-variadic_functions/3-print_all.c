8dd232de89b8cdda3baa6a12f8051529d04ec2de
Kalkidan-Amare <kalkidanamare11a@gmail.com>
2023-07-24 05:55:17 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,89 +1,50 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
-
+#include <stdarg.h>
 /**
-	* get_length - gives length of arguments from format
-	* @format: format to get length of
-	* Description: gives length of arguments from format
-	* Return: int
-*/
-int get_length(const char * const format)
-{
-	int j = 0;
-	int length = 0;
-
-	while (format[j] != '\0')
-	{
-		switch (format[j])
-		{
-			case 'c':
-				length++;
-				break;
-			case 'i':
-				length++;
-				break;
-			case 'f':
-				length++;
-				break;
-			case 's':
-				length++;
-				break;
-			default:
-				j++;
-				continue;
-				break;
-		}
-		j++;
-	}
-	return (length);
-}
-
-/**
-	* print_all - prints any parameter given to it
-	* @format: datatype of parameters passed to function
-	* Description: prints any parameter given to it
-	* Return: void
-*/
+ * print_all - Entry Point
+ * c = char, i = int, f = float, s = char * (if null print (nil))
+ * @format: list of arg types
+ * Return: 0
+ */
 void print_all(const char * const format, ...)
 {
-	int i = 0;
-	va_list ap;
-	char *s;
-	int length = get_length(format);
-	int j = 0;
+	va_list valist;
+	int n = 0, i = 0;
+	char *sep = ", ";
+	char *str;
 
-	va_start(ap, format);
-	while (format[i] != '\0' && format != NULL)
-	{
-		switch (format[i])
-		{
-			case 'c':
-				printf("%c", va_arg(ap, int));
-				break;
-			case 'i':
-				printf("%i", va_arg(ap, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(ap, double));
-				break;
-			case 's':
-				s = va_arg(ap, char *);
-				if (s == NULL)
-				{
-					printf("(nil)");
-					break;
-				}
-				printf("%s", s);
-				break;
-			default:
-				i++;
-				continue;
-		}
-		j++;
-		if (j < length)
-			printf(", ");
+	va_start(valist, format);
+
+	while (format && format[i])
 		i++;
+
+	while (format && format[n])
+	{
+		if (n  == (i - 1))
+		{
+			sep = "";
+		}
+		switch (format[n])
+		{
+		case 'c':
+			printf("%c%s", va_arg(valist, int), sep);
+			break;
+		case 'i':
+			printf("%d%s", va_arg(valist, int), sep);
+			break;
+		case 'f':
+			printf("%f%s", va_arg(valist, double), sep);
+			break;
+		case 's':
+			str = va_arg(valist, char *);
+			if (str == NULL)
+				str = "(nil)";
+			printf("%s%s", str, sep);
+			break;
+		}
+		n++;
 	}
 	printf("\n");
+	va_end(valist);
 }
