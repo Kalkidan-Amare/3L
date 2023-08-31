e83887ae1fe9f32a45510ef4b0670656f4e09362
Kalkidan-Amare <kalkidanamare11a@gmail.com>
2023-08-31 04:59:54 -0700

    betty

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,14 +6,12 @@
   *return:
   */
 
-int factorial(int n){
-	if(n<0){
-	return -1;
-	}
-	else if(n==0){
-	return 0;
-	}
-	else{
-	return n*(n-1);
-	}
+int factorial(int n)
+{
+	if(n < 0)
+		return -1;
+	else if(n == 0)
+		return 0;
+	else
+		return n * factorial(n - 1);
 }
